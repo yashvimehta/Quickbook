3f72da20288afe05404846c7449561e86e5414db
yashvimehta <yashvimehta45@gmail.com>
2022-09-29 12:50:12 +0530

    Camera Setup

--- app/src/main/java/com/example/quickbook/HomePage.java
+++ app/src/main/java/com/example/quickbook/HomePage.java
@@ -1,14 +1,53 @@
 package com.example.quickbook;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
+
+    ImageView cameraImageView;
+    public static final int CAMERA_REQUEST=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        cameraImageView = findViewById(R.id.cameraImageView);
+    }
+
+    public void openCamera ( View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_REQUEST){
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            cameraImageView.setImageBitmap(bitmap);
+        }
     }
 }
