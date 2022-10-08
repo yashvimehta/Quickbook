6650f7778a7cd491d3cc5111b0751db51e1fabb9
yashvimehta <yashvimehta45@gmail.com>
2022-10-08 16:04:14 +0530

    Storing Image Internally

--- app/src/main/java/com/example/quickbook/HomePage.java
+++ app/src/main/java/com/example/quickbook/HomePage.java
@@ -4,19 +4,27 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -45,6 +53,8 @@ public class HomePage extends AppCompatActivity {
         logoImageView = findViewById(R.id.logoImageView);
         cameraImageView = findViewById(R.id.cameraImageView);
         messageTextView = findViewById(R.id.messageTextView);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
 
     }
 
@@ -59,22 +69,36 @@ public class HomePage extends AppCompatActivity {
         if (requestCode == CAMERA_REQUEST) {
             Bitmap bitmap = (Bitmap)data.getExtras().get("data");
             imageImageView.setImageBitmap(bitmap);
+            ContextWrapper cw = new ContextWrapper(getApplicationContext());
+            File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+            File file = new File(directory, "UniqueFileName" + ".jpg");
+            if (!file.exists()) {
+                Log.d("path", file.toString());
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    fos.flush();
+                    fos.close();
+                    connectServer(file.toString());
+                } catch (java.io.IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
             logoImageView.setVisibility(View.INVISIBLE);
             imageImageView.setVisibility(View.VISIBLE);
             cameraImageView.setVisibility(View.INVISIBLE);
             messageTextView.setVisibility(View.INVISIBLE);
 
-
-            connectServer(bitmap);
         }
     }
 
-    void connectServer(Bitmap bitmap){
-
+    void connectServer(String file){
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.RGB_565;
+        Bitmap bitmap = BitmapFactory.decodeFile(file, options);
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
         byte[] byteArray = stream.toByteArray();
         String postUrl1= "http://20.219.149.149:5000/get_book_data_api";
@@ -87,7 +111,54 @@ public class HomePage extends AppCompatActivity {
 
         postRequest(postUrl1, postBodyImage);
         postRequest(postUrl2, postBodyImage);
+//        String path = MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(), bitmap, "Title", null);
+//        Uri uri= Uri.parse(path);
+//        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+//        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+//        File file = new File(directory, "UniqueFileName" + ".jpg");
+//        if (!file.exists()) {
+//            Log.d("path", file.toString());
+//            FileOutputStream fos = null;
+//            try {
+//                fos = new FileOutputStream(file);
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+//                fos.flush();
+//                fos.close();
+//                readFile(file.toString());
+//            } catch (java.io.IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
+
+//    void readFile(String path){
+//        File file = new File(path);
+//        int size = (int) file.length();
+//        byte[] bytes = new byte[size];
+//        try {
+//            InputStream buf = new ByteArrayInputStream(file.getBytes());
+//            buf.read(bytes, 0, bytes.length);
+//            buf.close();
+//            byte[] byteArray = buf.readAllBytes();
+//            String postUrl1= "http://20.219.149.149:5000/get_book_data_api";
+//            String postUrl2= "http://20.219.149.149:5000/identify";
+//
+//            RequestBody postBodyImage = new MultipartBody.Builder()
+//                    .setType(MultipartBody.FORM)
+//                    .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray))
+//                    .build();
+//
+//            postRequest(postUrl1, postBodyImage);
+//            postRequest(postUrl2, postBodyImage);
+//            buf.close();
+//        } catch (FileNotFoundException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
 
     void postRequest(String postUrl, RequestBody postBody) {
 
