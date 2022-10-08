fb231d4aaa877138c77e5d551753ecad7d567cb4
yashvimehta <yashvimehta45@gmail.com>
2022-10-08 19:07:31 +0530

    Added Gallery Option 3

--- app/src/main/java/com/example/quickbook/HomePage.java
+++ app/src/main/java/com/example/quickbook/HomePage.java
@@ -101,14 +101,14 @@ public class HomePage extends AppCompatActivity {
             messageTextView.setVisibility(View.INVISIBLE);
 
         }
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
+        else if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+//            Uri selectedImage = data.getData();
+//            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+//            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+//            cursor.moveToFirst();
+//            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//            String picturePath = cursor.getString(columnIndex);
+//            cursor.close();
             Bitmap bitmap = (Bitmap)data.getExtras().get("data");
             imageImageView.setImageBitmap(bitmap);
             logoImageView.setVisibility(View.INVISIBLE);
