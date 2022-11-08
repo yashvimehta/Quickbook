ac6c0df617f1b280048ce2d45d6d0f1c8d832f0c
yashvimehta <yashvimehta45@gmail.com>
2022-11-08 21:27:23 +0530

    Partially Setting Fragments done

--- app/src/main/java/com/example/quickbook/FragmentSetAdmin/SettingsFragment.java
+++ app/src/main/java/com/example/quickbook/FragmentSetAdmin/SettingsFragment.java
@@ -1,12 +1,14 @@
 package com.example.quickbook.FragmentSetAdmin;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +18,24 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.quickbook.HomePage;
 import com.example.quickbook.R;
+import com.example.quickbook.SignUp;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SettingsFragment extends Fragment {
@@ -29,14 +47,32 @@ public class SettingsFragment extends Fragment {
     EditText pwdInputText;
     EditText confirmPwdInputText;
     Button editProfileButton;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        firebaseAuth = FirebaseAuth.getInstance();
         final View view = inflater.inflate(R.layout.fragment_settings, container, false);
         issueDurationInputText=view.findViewById(R.id.issueDurationInputText);
         consecutiveIssualsInputText=view.findViewById(R.id.consecutiveIssualsInputText);
         fineInputText=view.findViewById(R.id.fineInputText);
         confirmRulesButton=view.findViewById(R.id.confirmRulesButton);
-        //TODO set above input text values from db
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference rulesDocumentRef = db.collection("Rules").document("ruless");
+        rulesDocumentRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    consecutiveIssualsInputText.setText(String.valueOf(task.getResult().getData().get("consecutiveIssuals")));
+                    issueDurationInputText.setText(String.valueOf(task.getResult().getData().get("issueDuration(days)")));
+                    fineInputText.setText(String.valueOf(task.getResult().getData().get("fineAmount(perDay)")));
+                }
+            }
+        });
         //fetch from db
 
         emailInputText=view.findViewById(R.id.emailInputText);
@@ -65,7 +101,10 @@ public class SettingsFragment extends Fragment {
                     correct=false;
                 }
                 if(correct){
-                    //TODO Store values in DB
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    db.collection("Rules").document("ruless").update("issueDuration(days)", issueDuration);
+                    db.collection("Rules").document("ruless").update("consecutiveIssuals", consecutiveIssuals);
+                    db.collection("Rules").document("ruless").update("fineAmount(perDay)", fine);
                     Toast.makeText(getActivity(), "Values stored successfully", Toast.LENGTH_LONG).show();
                 }
                 else{
@@ -92,6 +131,29 @@ public class SettingsFragment extends Fragment {
                 }
                 if(correct){
                     //TODO Store values in DB
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    db.collection("Users").document(user.getUid()).update("email", email);
+                    db.collection("Users").document(user.getUid()).update("password", pwd);
+                    DocumentReference rulesDocumentRef = db.collection("Rules").document(user.getUid());
+                    rulesDocumentRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                Log.i("phone", String.valueOf(task.getResult().getData().get("Phone Number")));
+                            }
+                        }
+                    });
+//                    user.updateEmail(email)
+//                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<Void> task) {
+//                                    if (task.isSuccessful()) {
+//                                        Log.d("YAYAY", "User email address updated.");
+//                                    }
+//                                }
+//                            });
+
                     Toast.makeText(getActivity(), "Changes stored successfully", Toast.LENGTH_LONG).show();
                 }
                 else{
