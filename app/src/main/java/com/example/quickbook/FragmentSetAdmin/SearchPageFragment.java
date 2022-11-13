e426c9e910ae6ba7dfbc93636f1c8af17e747133
malharbangdiwala <malharbangdiwala03@gmail.com>
2022-11-13 13:02:07 +0530

    Search page error handling completed

--- app/src/main/java/com/example/quickbook/FragmentSetAdmin/SearchPageFragment.java
+++ app/src/main/java/com/example/quickbook/FragmentSetAdmin/SearchPageFragment.java
@@ -9,9 +9,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -63,6 +65,8 @@ public class SearchPageFragment extends Fragment {
         frameLayout = (FrameLayout) view.findViewById(R.id.frame);
         searchButton=view.findViewById(R.id.searchButton);
         booksListView=view.findViewById(R.id.booksListView);
+
+        
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -90,25 +94,34 @@ public class SearchPageFragment extends Fragment {
                                 SearchBookResult mResult = response.body();
                                 if (mResult.getSuccess()) {
                                     int length=mResult.getAnswer().size();
-                                    ArrayList<String[]> cardArrayList=new ArrayList<String[]>();
-                                    for(int i=0;i<length;i++){
-                                        HashMap<String, String> record = (HashMap<String, String>) mResult.getAnswer().get(i);
-                                        cardArrayList.add(new String[]{record.get("title"),record.get("authors"),record.get("publisher"),record.get("publishedDate"),record.get("description"),record.get("thumbnail"),record.get("ISBN")});
+                                    if(length==0){
+                                        Toast.makeText(getContext(), "No such book exists!", Toast.LENGTH_SHORT).show();
+                                        booksListView.setVisibility(View.INVISIBLE);
                                     }
-
-                                    mCustomCardAdapter = new SearchCardAdapter(requireContext(),cardArrayList);
-                                    booksListView.setAdapter(mCustomCardAdapter);
-                                    booksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                        @Override
-                                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                                            Log.i("success","Clicked on:"+cardArrayList.get(i)[0]);
-                                            Intent intent = new Intent(getActivity().getApplication(), BookInfo.class);
-                                            intent.putExtra("bookData",cardArrayList.get(i));
-                                            startActivity(intent);
+                                    else {
+                                        ArrayList<String[]> cardArrayList = new ArrayList<String[]>();
+                                        cardArrayList.clear();
+                                        for (int i = 0; i < length; i++) {
+                                            HashMap<String, String> record = (HashMap<String, String>) mResult.getAnswer().get(i);
+                                            cardArrayList.add(new String[]{record.get("title"), record.get("authors"), record.get("publisher"), record.get("publishedDate"), record.get("description"), record.get("thumbnail"), record.get("ISBN")});
 
 
+                                            mCustomCardAdapter = new SearchCardAdapter(requireContext(), cardArrayList);
+                                            booksListView.setAdapter(mCustomCardAdapter);
+                                            booksListView.setVisibility(View.VISIBLE);
+                                            booksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                                @Override
+                                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                                    Log.i("success", "Clicked on:" + cardArrayList.get(i)[0]);
+                                                    Intent intent = new Intent(getActivity().getApplication(), BookInfo.class);
+                                                    intent.putExtra("bookData", cardArrayList.get(i));
+                                                    startActivity(intent);
+
+
+                                                }
+                                            });
                                         }
-                                    });
+                                    }
                                 } else {
                                     Log.i("Success Checking", "" + mResult.getError());
                                 }
@@ -116,10 +129,12 @@ public class SearchPageFragment extends Fragment {
 
                             @Override
                             public void onFailure(Call<SearchBookResult> call, Throwable t) {
+                                Toast.makeText(getContext(), "Unable to fetch results from server", Toast.LENGTH_SHORT).show();
                                 Log.i("Failure Checking", "There was an error " + t.getMessage());
                             }
                         });
                     } catch (Exception e) {
+                        Toast.makeText(getContext(), "Unable to fetch results from server", Toast.LENGTH_SHORT).show();
                         e.printStackTrace();
                     }
                 }
