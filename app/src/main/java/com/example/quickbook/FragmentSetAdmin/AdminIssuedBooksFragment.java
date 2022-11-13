22aee24ed938675277944d1fc9f4f91b516a8825
yashvimehta <yashvimehta45@gmail.com>
2022-11-13 13:39:59 +0530

    Added search issue book by ID

--- app/src/main/java/com/example/quickbook/FragmentSetAdmin/AdminIssuedBooksFragment.java
+++ app/src/main/java/com/example/quickbook/FragmentSetAdmin/AdminIssuedBooksFragment.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.quickbook.R;
@@ -30,6 +31,7 @@ import java.util.Date;
 public class AdminIssuedBooksFragment extends Fragment {
 
     ListView mListView;
+    EditText searchByID;
     CustomCardAdapter mCustomCardAdapter;
 
     @Override
@@ -43,7 +45,9 @@ public class AdminIssuedBooksFragment extends Fragment {
         final View view = inflater.inflate(R.layout.fragment_admin_issued_books, container, false);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        mListView = view.findViewById(R.id.similarItemsListView);
+        mListView = view.findViewById(R.id.issuedBooks);
+
+
 
         db.collection("Transactions")
                 .get()
@@ -74,5 +78,38 @@ public class AdminIssuedBooksFragment extends Fragment {
                 });
         Log.i("setttt", "ppp");
         return view;
+    }
+
+    public void onSearchClick(View view){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        searchByID = view.findViewById(R.id.searchByID);
+        String searchID = searchByID.getText().toString();
+        db.collection("Transactions")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        int val = 0;
+                        if (task.isSuccessful()) {
+                            ArrayList<String[]> stringArrayList=new ArrayList<String[]>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String bookName = String.valueOf(document.getData().get("bookName"));
+                                String issuerID = String.valueOf(document.getData().get("issuerID"));
+                                Timestamp javaDate1 = (Timestamp) document.getData().get("returnDate");
+                                Date javaDate = javaDate1.toDate();
+                                String[] returnDate  = String.valueOf(javaDate).split(" GMT") ;
+                                String returnn = returnDate[0].substring(0, returnDate[0].length() - 9);
+//                                Log.i("date", returnn);
+                                if(searchID.equals(issuerID)){
+                                    stringArrayList.add(new String[]{bookName , issuerID , returnn});
+                                }
+                            }
+                            mCustomCardAdapter = new CustomCardAdapter(requireContext(),stringArrayList);
+                            mListView.setAdapter(mCustomCardAdapter);
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
