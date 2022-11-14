5e82a4b271319696b15f893f3133a7efc8b564c3
malharbangdiwala <malharbangdiwala03@gmail.com>
2022-11-14 19:04:50 +0530

    Admin Issued Books Fragment logic completed

--- app/src/main/java/com/example/quickbook/FragmentSetAdmin/AdminIssuedBooksFragment.java
+++ app/src/main/java/com/example/quickbook/FragmentSetAdmin/AdminIssuedBooksFragment.java
@@ -2,29 +2,21 @@ package com.example.quickbook.FragmentSetAdmin;
 
 import static android.content.ContentValues.TAG;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.work.Data;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.example.quickbook.ApiHelper.ApiInterface;
-import com.example.quickbook.ApiHelper.SearchBookResult;
-import com.example.quickbook.BookInfo;
+
 import com.example.quickbook.R;
-import com.example.quickbook.ResultsActivity;
-import com.example.quickbook.SearchCardAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
@@ -34,15 +26,6 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AdminIssuedBooksFragment extends Fragment {
 
@@ -50,7 +33,7 @@ public class AdminIssuedBooksFragment extends Fragment {
     EditText searchByID;
     CustomCardAdapter mCustomCardAdapter;
     Button searchButton;
-
+    FirebaseFirestore db;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,7 +44,7 @@ public class AdminIssuedBooksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_admin_issued_books, container, false);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db= FirebaseFirestore.getInstance();
         mListView = view.findViewById(R.id.issuedBooks);
         searchByID = view.findViewById(R.id.searchByID);
         searchButton=view.findViewById(R.id.searchButton);
@@ -71,44 +54,21 @@ public class AdminIssuedBooksFragment extends Fragment {
             public void onClick(View view) {
                 String searchName = searchByID.getText().toString();
                 if(searchName.equals("")){
-                    Toast.makeText(getContext(), "Member ID cannot be empty", Toast.LENGTH_SHORT).show();
+                    setAllData("");
                 }
                 else {
-                    db.collection("Transactions")
-                            .get()
-                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                    int val = 0;
-                                    if (task.isSuccessful()) {
-                                        ArrayList<String[]> stringArrayList = new ArrayList<String[]>();
-                                        for (QueryDocumentSnapshot document : task.getResult()) {
-                                            String bookName = String.valueOf(document.getData().get("bookName"));
-                                            String issuerID = String.valueOf(document.getData().get("issuerID"));
-                                            Timestamp javaDate1 = (Timestamp) document.getData().get("returnDate");
-                                            Date javaDate = javaDate1.toDate();
-                                            String[] returnDate = String.valueOf(javaDate).split(" GMT");
-                                            String returnn = returnDate[0].substring(0, returnDate[0].length() - 9);
-                                            if (searchName.equals(issuerID)) {
-                                                stringArrayList.add(new String[]{bookName, issuerID, returnn});
-                                                val++;
-                                            }
-                                        }
-                                        if (val == 0) {
-                                            Toast.makeText(getContext(), "Member ID not found", Toast.LENGTH_SHORT).show();
-                                        }
-                                        mCustomCardAdapter = new CustomCardAdapter(requireContext(), stringArrayList);
-                                        mListView.setAdapter(mCustomCardAdapter);
-                                    } else {
-                                        Log.d(TAG, "Error getting documents: ", task.getException());
-                                    }
-                                }
-                            });
+                    setAllData(searchName);
                 }
             }
         });
 
+        setAllData("");
+        Log.i("setttt", "ppp");
+        return view;
+    }
 
+    public void setAllData(String searchName){
+        int val=0;
         db.collection("Transactions")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -124,10 +84,13 @@ public class AdminIssuedBooksFragment extends Fragment {
                                 Date javaDate = javaDate1.toDate();
                                 String[] returnDate  = String.valueOf(javaDate).split(" GMT") ;
                                 String returnn = returnDate[0].substring(0, returnDate[0].length() - 9);
-                                Log.i("date", returnn);
-                                stringArrayList.add(new String[]{bookName , issuerID , returnn});
-                                Log.i("set ", "yayayay");
-                                Log.i("set1 ", "yayayay");
+                                if (searchName.equals("")||searchName.equals(issuerID)) {
+                                    stringArrayList.add(new String[]{bookName, issuerID, returnn});
+                                    val++;
+                                }
+                            }
+                            if (val == 0) {
+                                Toast.makeText(getContext(), "Member ID not found", Toast.LENGTH_SHORT).show();
                             }
                             mCustomCardAdapter = new CustomCardAdapter(requireContext(),stringArrayList);
                             mListView.setAdapter(mCustomCardAdapter);
@@ -136,7 +99,5 @@ public class AdminIssuedBooksFragment extends Fragment {
                         }
                     }
                 });
-        Log.i("setttt", "ppp");
-        return view;
     }
 }
