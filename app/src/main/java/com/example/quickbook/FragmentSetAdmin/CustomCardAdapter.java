f3ff50372e31a8b22dd5a180299a5e2226b7614e
malharbangdiwala <malharbangdiwala03@gmail.com>
2022-11-14 12:06:16 +0530

    Added Alert Box for fine amount

--- app/src/main/java/com/example/quickbook/FragmentSetAdmin/CustomCardAdapter.java
+++ app/src/main/java/com/example/quickbook/FragmentSetAdmin/CustomCardAdapter.java
@@ -2,14 +2,19 @@ package com.example.quickbook.FragmentSetAdmin;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 
 //import com.bumptech.glide.Glide;
 //import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -24,6 +29,8 @@ public class CustomCardAdapter extends ArrayAdapter<String[]> {
     Context mContext;
     ArrayList<String[]>mArrayList;
     boolean showLikes;
+    Button endIssueButton;
+    EditText setLateFeeEditText;
 
     public CustomCardAdapter(@NonNull Context context, ArrayList<String[]> stringArrayList) {
         super(context, R.layout.custom_card, stringArrayList);
@@ -41,10 +48,50 @@ public class CustomCardAdapter extends ArrayAdapter<String[]> {
         TextView bookName = view.findViewById(R.id.bookName);
         TextView ID = view.findViewById(R.id.ID);
         TextView returnDate = view.findViewById(R.id.returnDate);
-
+        endIssueButton=view.findViewById(R.id.endIssue);
         bookName.setText(mArrayList.get(position)[0]);
         ID.setText("Member ID: "+mArrayList.get(position)[1] );
         returnDate.setText("Return Date: "+mArrayList.get(position)[2]);
+
+
+        endIssueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext(),R.style.AlertDialogTheme);
+                builder.setTitle("Set late fee amount");
+                final View customLayout = inflater
+                        .inflate(R.layout.alert_admin_set_fine, null);
+                builder.setView(customLayout);
+                builder.setPositiveButton(
+                        "Charge Fees",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                String feeValue=setLateFeeEditText.getText().toString();
+                                if(feeValue.equals("")||feeValue.equals("0")){
+                                    Toast.makeText(getContext(), "No fees charged", Toast.LENGTH_SHORT).show();
+                                    //TODO save lateFee=0 in DB for this transaction.
+                                }
+                                else {
+                                    Toast.makeText(getContext(), "Fees has been charged", Toast.LENGTH_SHORT).show();
+                                    //TODO Get value from editText and save in DB for this transaction.
+                                }
+                                }
+                        });
+                builder.setNegativeButton("Cancel return", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                setLateFeeEditText = customLayout.findViewById(R.id.setLateFeeEditText);
+                //TODO Calculate late fees and display in editText
+                setLateFeeEditText.setText("100");
+                dialog.show();
+            }});
 
         return view;
     }
